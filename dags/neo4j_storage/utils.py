7ef6e05c7a9c83be28e25d5571b0b61b9d4647b6
Mohammad Twin <scientists.twin@gmail.com>
2023-11-29 11:22:15 +0400

    feat: wrote a function for flatMap over a dictionary

--- dags/neo4j_storage/utils.py
+++ dags/neo4j_storage/utils.py
@@ -17,3 +17,68 @@ def remove_nested_collections(data):
             del data[key]
     
     return data
+
+def flat_map(obj):
+    """
+    Function to iterate over an object and flat map its values if the value is an object or an array.
+
+    The function works recursively, handling nested dictionaries and lists. For each nested
+    element, it creates a flattened key based on the path to that element.
+
+    Parameters:
+    obj (dict or list): The object to be flattened.
+
+    Returns:
+    dict: A dictionary with flattened keys and values.
+
+    Example:
+    Given an object like:
+    ```
+    {
+        "name": "John",
+        "address": {
+            "street": "Main St",
+            "city": "Springfield"
+        },
+        "phones": ["123-456-7890", "987-654-3210"]
+    }
+    ```
+
+    The function will return:
+    ```
+    {
+        'name': 'John',
+        'address.street': 'Main St',
+        'address.city': 'Springfield',
+        'phones.0': '123-456-7890',
+        'phones.1': '987-654-3210'
+    }
+    ```
+    """
+    if isinstance(obj, dict):
+        # For dictionaries, iterate through each key-value pair
+        flat = {}
+        for key, value in obj.items():
+            # Recursively flat map the value if it's a dictionary or list
+            if isinstance(value, (dict, list)):
+                for inner_key, inner_value in flat_map(value).items():
+                    # Combine the outer key with the inner key
+                    flat[f"{key}.{inner_key}"] = inner_value
+            else:
+                flat[key] = value
+        return flat
+    elif isinstance(obj, list):
+        # For lists, iterate through each element
+        flat = {}
+        for index, value in enumerate(obj):
+            # Recursively flat map the value if it's a dictionary or list
+            if isinstance(value, (dict, list)):
+                for inner_key, inner_value in flat_map(value).items():
+                    # Use the index as the key
+                    flat[f"{index}.{inner_key}"] = inner_value
+            else:
+                flat[index] = value
+        return flat
+    else:
+        # If the object is neither a dictionary nor a list, return it as is
+        return {0: obj}
