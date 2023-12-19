2d19614c76559806ae09279956c8dc8722e8e930
Mohammad Twin <scientists.twin@gmail.com>
2023-12-19 14:28:37 +0400

    feat: docker-entrypoint was updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-echo "chang dir to dags"
-cd dags || exit
-
-python3 -m coverage run --omit=tests/* -m pytest tests
-
-cp .coverage ../.coverage
-cd .. || exit
+python3 -m coverage run --omit=tests/* -m pytest .
 python3 -m coverage lcov -i -o coverage/lcov.info
