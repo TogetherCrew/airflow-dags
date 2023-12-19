3ce2033bc9c7727c4eb0be10639c38dc5faefb5f
Mohammad Amin <dadgaramin96@gmail.com>
2023-12-19 17:48:29 +0330

    fix: pylint linter issue!

--- dags/hivemind_etl.py
+++ dags/hivemind_etl.py
@@ -62,7 +62,7 @@ with DAG(
         process_discord_guild_mongo(community_id=community_id)
         logging.info(f"Community {community_id} Job finished!")
 
-    communities = get_all_discord_communities()
+    communities = get_discord_communities()
     # `start_discord_vectorstore` will be called multiple times
     # with the length of the list
     start_discord_vectorstore.expand(community_id=communities)
@@ -70,9 +70,11 @@ with DAG(
 with DAG(dag_id="discord_summary_vector_store", start_date=datetime(2023, 1, 1)) as dag:
 
     @task
-    def get_all_discord_communities() -> list[str]:
+    def get_mongo_discord_communities() -> list[str]:
         """
         Getting all communities having discord from database
+        this function is the same with `get_discord_communities`
+        we just changed the name for the pylint
         """
         communities = get_all_discord_communities()
         return communities
@@ -83,5 +85,5 @@ with DAG(dag_id="discord_summary_vector_store", start_date=datetime(2023, 1, 1))
         process_discord_summaries(community_id=community_id, verbose=False)
         logging.info(f"Community {community_id} Job finished!")
 
-    communities = get_all_discord_communities()
+    communities = get_mongo_discord_communities()
     start_discord_summary_vectorstore.expand(community_id=communities)
