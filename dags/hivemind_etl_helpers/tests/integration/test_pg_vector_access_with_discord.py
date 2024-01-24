227ef98498b06d67613ab5d59af64880a46927f3
Mohammad Amin <dadgaramin96@gmail.com>
2024-01-24 13:31:31 +0330

    fix: test case, same as previous one!

--- dags/hivemind_etl_helpers/tests/integration/test_pg_vector_access_with_discord.py
+++ dags/hivemind_etl_helpers/tests/integration/test_pg_vector_access_with_discord.py
@@ -59,7 +59,7 @@ class TestPGVectorAccess(unittest.TestCase):
                     "platforms": [
                         {
                             "platformId": platform_id,
-                            "fromDate": datetime(2023, 10, 1),
+                            "fromDate": datetime(2023, 1, 1),
                             "options": {
                                 "channels": channels,
                                 "roles": ["role_id"],
