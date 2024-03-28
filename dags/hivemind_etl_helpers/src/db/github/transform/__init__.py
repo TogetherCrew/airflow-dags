c71f5920a3a16e2fb7ad5bcc5d9c6e5689fd444b
Mohammad Amin <dadgaramin96@gmail.com>
2024-03-28 09:23:45 +0330

    feat: Adding the GitHubTransformation class!

--- dags/hivemind_etl_helpers/src/db/github/transform/__init__.py
+++ dags/hivemind_etl_helpers/src/db/github/transform/__init__.py
@@ -1,5 +1,2 @@
 # flake8: noqa
-from .comments import transform_comments
-from .commits import transform_commits
-from .issues import transform_issues
-from .pull_requests import transform_prs
+from .transform import GitHubTransformation
