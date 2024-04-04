7194e2170aa714345ea66f07bdd21d48429911f9
geewynn <ekainugodwin@gmail.com>
2024-04-04 14:41:31 +0100

    fix: exec query

--- dags/hivemind_etl_helpers/src/db/github/extract/commit.py
+++ dags/hivemind_etl_helpers/src/db/github/extract/commit.py
@@ -3,14 +3,15 @@ import neo4j
 from github.neo4j_storage.neo4j_connection import Neo4jConnection
 from hivemind_etl_helpers.src.db.github.schema import GitHubCommit
 
+
 class GithubCommitExtraction:
     def __init__(self) -> None:
         self.neo4j_connection = Neo4jConnection()
-    
+
     def fetch_commits(
-    self,
-    repository_id: list[int],
-    from_date: datetime | None = None,
+        self,
+        repository_id: list[int],
+        from_date: datetime | None = None,
     ) -> list[GitHubCommit]:
         """
         fetch commits from data dump in neo4j
@@ -36,7 +37,7 @@ class GithubCommitExtraction:
             github_commits.append(issue)
 
         return github_commits
-    
+
     def _fetch_raw_commits(
         self,
         repository_id: list[int],
@@ -82,10 +83,12 @@ class GithubCommitExtraction:
             ORDER BY created_at
         """
 
-        with self._neo4j_connection.connect_neo4j().session() as session:
-            raw_records = session.execute_read(self._exec_query, repoIds=repository_id, from_date=from_date)
-        return raw_records
+        def _exec_query(tx, repoIds, from_date):
+            result = tx.run(query, repoIds=repoIds, from_date=from_date)
+            return list(result)
 
-    def _exec_query(tx, repoIds, from_date):
-        result = tx.run(query, repoIds=repoIds, from_date=from_date)
-        return list(result)
+        with self.neo4j_connection.connect_neo4j().session() as session:
+            raw_records = session.execute_read(
+                _exec_query, repoIds=repository_id, from_date=from_date
+            )
+        return raw_records
