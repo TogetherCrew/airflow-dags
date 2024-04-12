4d6e3f6047ab739638be8624d31506ff8afa57e6
Jacob <billings.jacob@gmail.com>
2024-04-12 18:21:29 +0200

    Draft commit, Bounty 2, Linted

--- dags/hivemind_etl_helpers/src/db/github/schema/__init__.py
+++ dags/hivemind_etl_helpers/src/db/github/schema/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 from .comment import GitHubComment
 from .commit import GitHubCommit
-from .issue import GitHubIssue, GitHubIssueID
+from .issue import GitHubIssue
 from .pull_request import GitHubPullRequest
