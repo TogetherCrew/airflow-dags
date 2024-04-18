3dc679dfeb0540b63cc3faade4fbc70d2a0d70b8
Jacob <billings.jacob@gmail.com>
2024-04-18 13:02:59 +0200

    Bounty 1 (&2) recommended fixes, + bug fixes

--- dags/hivemind_etl_helpers/src/db/github/extract/__init__.py
+++ dags/hivemind_etl_helpers/src/db/github/extract/__init__.py
@@ -1,7 +1,6 @@
 # flake8: noqa
 from .comments import fetch_comments
 from .commit import fetch_commits
+from .github_extraction import GithubExtraction
 from .issues import GithubIssueExtraction
 from .pull_requests import fetch_pull_requests
-
-from .github_extraction import GithubExtraction
--- dags/hivemind_etl_helpers/src/db/github/extract/github_extraction.py
+++ dags/hivemind_etl_helpers/src/db/github/extract/github_extraction.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+import neo4j
 from hivemind_etl_helpers.src.db.github.extract import GithubIssueExtraction
 from hivemind_etl_helpers.src.db.github.schema import GitHubIssue
+
 
 class GithubExtraction:
     def __init__(self):
@@ -12,12 +14,19 @@ class GithubExtraction:
         # self.comment_extraction = GitHubCommentExtraction()
         self.issue_extraction = GithubIssueExtraction()
 
-    def fetch_issues(
-            self,
-            repository_id: list[int],
-            from_date: datetime | None = None,
-            **kwargs
-    ) -> list[GitHubIssue]:
-        return self.issue_extraction.fetch_issues(
+    def _fetch_raw_issues(
+        self, repository_id: list[int], from_date: datetime | None = None, **kwargs
+    ) -> list[neo4j._data.Record]:
+        return self.issue_extraction._fetch_raw_issues(
             repository_id, from_date, **kwargs
         )
+
+    def fetch_issues(
+        self, repository_id: list[int], from_date: datetime | None = None, **kwargs
+    ) -> list[GitHubIssue]:
+        return self.issue_extraction.fetch_issues(repository_id, from_date, **kwargs)
+
+    def fetch_issue_ids(
+        self, repository_id: list[int], from_date: datetime | None = None, **kwargs
+    ) -> list[GitHubIssue]:
+        return self.issue_extraction.fetch_issue_ids(repository_id, from_date, **kwargs)
