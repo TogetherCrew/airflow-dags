78f8722d85f201cfef59bc092005b370a961163e
geewynn <ekainugodwin@gmail.com>
2024-04-25 13:26:50 +0100

    fix: fix format

--- dags/hivemind_etl_helpers/src/db/gdrive/gdrive_loader.py
+++ dags/hivemind_etl_helpers/src/db/gdrive/gdrive_loader.py
@@ -4,8 +4,6 @@ from typing import List, Optional
 from llama_index.core.schema import Document
 from llama_index.readers.google import GoogleDriveReader
 
-logging.basicConfig(level=logging.INFO)
-
 
 class GoogleDriveLoader:
     def __init__(self, client_config):
@@ -22,29 +20,56 @@ class GoogleDriveLoader:
         Args:
             drive_ids: List of the google drive ids.
             folder_id: List of the google folder ids.
-            file_ids: List of google drive file ids.
+            file_ids: List of google file ids.
 
         Returns:
            List of loaded document objects.
         """
+        loader = GoogleDriveReader()
 
-        loader = GoogleDriveReader(self.client_config)
-
-        all_docs = []
         if folder_ids:
-            logging.info("Loading documents from folders...")  # Log start of process
-            for folder_id in folder_ids:
-                logging.info(f"Processing folder: {folder_id}")  # Log each folder
-                all_docs.extend(loader.load_data(folder_id=folder_id))
+            logging.info("Loading documents from folders...")
+            return self._load_from_folders(loader, folder_ids)
         elif drive_ids:
             logging.info("Loading documents from drives...")
-            for drive_id in drive_ids:
-                logging.info(f"Processing drive: {drive_id}")
-                all_docs.extend(loader.load_data(folder_id=drive_id))
+            return self._load_from_drives(loader, drive_ids)
         elif file_ids:
             logging.info("Loading documents directly...")
-            all_docs = loader.load_data(file_ids=file_ids)
+            return self._load_from_files(loader, file_ids)
         else:
             raise ValueError("One input at least must be given!")
 
-        return all_docs
+    def _load_from_folders(self, loader: GoogleDriveReader, folder_ids: List[str]):
+        folders_data = []
+        for folder_id in folder_ids:
+            logging.info(f"Processing folder: {folder_id}")
+            try:
+                folders_data.extend(loader.load_data(folder_id=folder_id))
+            except Exception as e:
+                logging.error(
+                    f"An error occurred while loading from folder: {e}", exc_info=True
+                )
+        return folders_data
+
+    def _load_from_drives(self, loader: GoogleDriveReader, drive_ids: List[str]):
+        drive_data = []
+        for drive_id in drive_ids:
+            logging.info(f"Processing drive: {drive_id}")
+            try:
+                drive_ids.extend(loader.load_data(folder_id=drive_ids))
+            except Exception as e:
+                logging.error(
+                    f"An error occurred while loading from folder: {e}", exc_info=True
+                )
+        return drive_data
+
+    def _load_from_files(self, loader: GoogleDriveReader, file_ids: List[str]):
+        file_data = []
+        logging.info(f"Processing file")
+        try:
+            file_data.extend(loader.load_data(file_ids=file_ids))
+        except Exception as e:
+            logging.error(
+                f"An error occurred while loading from file: {e}", exc_info=True
+            )
+        return file_data
