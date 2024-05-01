888a08590328de6cc40b5a75dd76817e4ad1f279
Mohammad Amin <dadgaramin96@gmail.com>
2024-05-01 13:49:27 +0330

    fix: lint error trying more

--- dags/discord_analyzer_etl.py
+++ dags/discord_analyzer_etl.py
@@ -34,9 +34,9 @@ with DAG(
 
         """
         # the platform that needs to be recomputed
-        platform_id_recompute = kwargs["dag_run"].conf.get(
+        platform_id_recompute = kwargs["dag_run"].conf.get(  # noqa: F841
             "recompute_platform", None
-        )  # noqa: F841
+        )
 
         # for default we're setting the recompute for all platforms to False
         # if an id for `recompute_platform` was given
