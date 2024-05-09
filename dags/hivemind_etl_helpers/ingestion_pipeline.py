2b82a93b349104fcfd1b9ec572c4b7bbb572ed1e
polux0 <afidelo@protonmail.com>
2024-05-09 10:33:47 +0200

    fix: resolving requested changes;

--- dags/hivemind_etl_helpers/ingestion_pipeline.py
+++ dags/hivemind_etl_helpers/ingestion_pipeline.py
@@ -1,6 +1,4 @@
-import os
-
-from dotenv import load_dotenv
+from hivemind_etl_helpers.src.utils.credentials import load_redis_credentials
 from llama_index.core import MockEmbedding
 from llama_index.core.ingestion import (DocstoreStrategy, IngestionCache,
                                         IngestionPipeline)
@@ -9,6 +7,9 @@ from llama_index.storage.docstore.postgres import PostgresDocumentStore
 from llama_index.storage.kvstore.redis import RedisKVStore as RedisCache
 from llama_index.vector_stores.postgres import PGVectorStore
 from tc_hivemind_backend.db.credentials import load_postgres_credentials
+from tc_hivemind_backend.db.utils.model_hyperparams import (
+    load_model_hyperparams
+)
 from tc_hivemind_backend.embeddings.cohere import CohereEmbedding
 
 from dags.hivemind_etl_helpers.src.db.gdrive.db_utils import setup_db
@@ -17,10 +18,10 @@ from dags.hivemind_etl_helpers.src.db.gdrive.db_utils import setup_db
 class CustomIngestionPipeline:
     def __init__(self, community_id: str, table_name: str,
                  testing: bool = False):
-        load_dotenv()
+        redis_credentials = load_redis_credentials()
         postgres_credentials = load_postgres_credentials()
-        self.redis_host = os.getenv("REDIS_HOST")
-        self.redis_port = os.getenv("REDIS_PORT")
+        self.redis_host = redis_credentials["host"]
+        self.redis_port = redis_credentials["port"]
 
         self.table_name = table_name
         self.dbname = f"community_{community_id}"
@@ -36,6 +37,7 @@ class CustomIngestionPipeline:
             self.embed_model = MockEmbedding(embed_dim=1024)
 
     def run_pipeline(self, docs):
+        _, embedding_dim = load_model_hyperparams()
         setup_db(community_id=self.community_id)
         pipeline = IngestionPipeline(
             transformations=[
@@ -51,12 +53,12 @@ class CustomIngestionPipeline:
                 **self.db_config,
                 database=self.dbname,
                 table_name=self.table_name,
-                embed_dim=1024,
+                embed_dim=embedding_dim,
             ),
             cache=IngestionCache(
                 cache=RedisCache.from_host_and_port(self.redis_host,
                                                     self.redis_port),
-                collection=self.dbname + "_ingestion_cache",
+                collection=self.dbname + f"_{self.table_name}" + "_ingestion_cache",
             ),
             docstore_strategy=DocstoreStrategy.UPSERTS,
         )
