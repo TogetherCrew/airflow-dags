43f0dd9b4bd2978382d7a39526b8972830aef0d5
Mohammad Amin <dadgaramin96@gmail.com>
2024-05-09 15:50:53 +0330

    feat: updated package names for better readability!

--- dags/hivemind_etl_helpers/src/utils/modules/__init__.py
+++ dags/hivemind_etl_helpers/src/utils/modules/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
+from .discord import ModulesDiscord
+from .discourse import ModulesDiscourse
 from .gdrive import ModulesGDrive
-from .modules_discord import ModulesDiscord
-from .modules_discourse import ModulesDiscourse
-from .modules_github import ModulesGitHub
+from .github import ModulesGitHub
