005c070ace09de1dfba4e5c3646d3c5c88f07b5a
geewynn <ekainugodwin@gmail.com>
2024-05-09 09:15:38 +0100

    fix: load embed, remove hardcode on coll and embed

--- dags/hivemind_etl_helpers/gdrive_ingestion_etl.py
+++ dags/hivemind_etl_helpers/gdrive_ingestion_etl.py
@@ -11,6 +11,7 @@ from llama_index.storage.docstore.postgres import PostgresDocumentStore
 from llama_index.storage.kvstore.redis import RedisKVStore as RedisCache
 from llama_index.vector_stores.postgres import PGVectorStore
 from tc_hivemind_backend.db.credentials import load_postgres_credentials
+from tc_hivemind_backend.db.utils.model_hyperparams import load_model_hyperparams
 from tc_hivemind_backend.embeddings.cohere import CohereEmbedding
 
 
@@ -20,6 +21,7 @@ class GoogleDriveIngestionPipeline:
 
         # Embedding models
         self.cohere_model = CohereEmbedding()
+        _, self.embedding_dim = load_model_hyperparams()
         self.pg_creds = load_postgres_credentials()
         self.redis_cred = load_redis_credentials()
         self.table_name = "gdrive"
@@ -50,11 +52,11 @@ class GoogleDriveIngestionPipeline:
                 user=self.pg_creds["user"],
                 password=self.pg_creds["password"],
                 table_name=self.table_name,
-                embed_dim=1024,
+                embed_dim=self.embedding_dim,
             ),
             cache=IngestionCache(
                 cache=RedisCache.from_host_and_port(self.redis_host, self.redis_port),
-                collection="sample_test_cache",
+                collection=self.dbname + f"_{self.table_name}" + "_ingestion_cache",
                 docstore_strategy=DocstoreStrategy.UPSERTS,
             ),
             docstore_strategy=DocstoreStrategy.UPSERTS,
