56f9d7c4b54b5e9c0cb1db20993bdde56da41b25
Mohammad Amin <dadgaramin96@gmail.com>
2024-05-14 15:04:16 +0330

    fix: test case with latest input params update!

--- dags/hivemind_etl_helpers/tests/integration/test_modules_base_query_token.py
+++ dags/hivemind_etl_helpers/tests/integration/test_modules_base_query_token.py
@@ -10,11 +10,30 @@ class TestModulesBaseQueryToken(TestCase):
     def setUp(self) -> None:
         self.client = MongoSingleton.get_instance().get_client()
         self.client["Core"].drop_collection("tokens")
+        self.client["Core"].drop_collection("platforms")
 
     def test_one_token(self):
         sample_user = ObjectId("5d7baf326c8a2e2400000000")
+        community_id = ObjectId("6579c364f1120850414e0dc5")
         sample_token_type = "type1"
         sample_token_value = "tokenid12345"
+        platform_id = ObjectId("6579c364f1120850414e0dc6")
+
+        self.client["Core"]["platforms"].insert_one(
+            {
+                "_id": platform_id,
+                "name": "platform_name",
+                "metadata": {
+                    "id": "113445975232201081511",
+                    "userId": str(sample_user),
+                },
+                "community": community_id,
+                "disconnectedAt": None,
+                "connectedAt": datetime.now(),
+                "createdAt": datetime.now(),
+                "updatedAt": datetime.now(),
+            }
+        )
 
         sample_token_doc = {
             "token": sample_token_value,
@@ -26,19 +45,24 @@ class TestModulesBaseQueryToken(TestCase):
             "updatedAt": datetime.now() - timedelta(days=1),
         }
         self.client["Core"]["tokens"].insert_one(sample_token_doc)
-        token = ModulesBase().get_token(user=sample_user, token_type=sample_token_type)
+        token = ModulesBase().get_token(
+            platform_id=platform_id, token_type=sample_token_type
+        )
 
         self.assertEqual(token, sample_token_value)
 
     def test_empty_tokens_collection(self):
         sample_user = ObjectId("5d7baf326c8a2e2400000000")
+        platform_id = ObjectId("6579c364f1120850414e0dc6")
         sample_token_type = "type1"
         with self.assertRaises(ValueError):
-            _ = ModulesBase().get_token(user=sample_user, token_type=sample_token_type)
+            _ = ModulesBase().get_token(
+                platform_id=platform_id, token_type=sample_token_type
+            )
 
-    def test_no_token(self):
+    def test_no_platform(self):
         sample_user = ObjectId("5d7baf326c8a2e2400000000")
-        user_with_no_token = ObjectId("5d7baf326c8a2e2400000001")
+        platform_id = ObjectId("6579c364f1120850414e0dc6")
         sample_token_type = "type1"
         sample_token_value = "tokenid12345"
 
@@ -54,5 +78,45 @@ class TestModulesBaseQueryToken(TestCase):
         self.client["Core"]["tokens"].insert_one(sample_token_doc)
         with self.assertRaises(ValueError):
             _ = ModulesBase().get_token(
-                user=user_with_no_token, token_type=sample_token_type
+                platform_id=platform_id, token_type=sample_token_type
+            )
+
+    def test_no_token(self):
+        sample_user = ObjectId("5d7baf326c8a2e2400000000")
+        sample_user_with_no_token = ObjectId("5d7baf326c8a2e2400000001")
+
+        platform_id = ObjectId("6579c364f1120850414e0dc6")
+        sample_token_type = "type1"
+        sample_token_value = "tokenid12345"
+        community_id = ObjectId("6579c364f1120850414e0dc5")
+
+        self.client["Core"]["platforms"].insert_one(
+            {
+                "_id": platform_id,
+                "name": "platform_name",
+                "metadata": {
+                    "id": "113445975232201081511",
+                    "userId": str(sample_user_with_no_token),
+                },
+                "community": community_id,
+                "disconnectedAt": None,
+                "connectedAt": datetime.now(),
+                "createdAt": datetime.now(),
+                "updatedAt": datetime.now(),
+            }
+        )
+
+        sample_token_doc = {
+            "token": sample_token_value,
+            "user": sample_user,
+            "type": sample_token_type,
+            "expires": datetime.now() + timedelta(days=1),
+            "blacklisted": False,
+            "createdAt": datetime.now() - timedelta(days=1),
+            "updatedAt": datetime.now() - timedelta(days=1),
+        }
+        self.client["Core"]["tokens"].insert_one(sample_token_doc)
+        with self.assertRaises(ValueError):
+            _ = ModulesBase().get_token(
+                platform_id=platform_id, token_type=sample_token_type
             )
