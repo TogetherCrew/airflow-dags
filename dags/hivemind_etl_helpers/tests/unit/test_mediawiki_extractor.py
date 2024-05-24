b1b650671c1b2ad683e6c28d6425b0c4cc33b50a
polux0 <afidelo@protonmail.com>
2024-05-24 10:03:27 +0200

    feat: adapting tests accordingly;

--- dags/hivemind_etl_helpers/tests/unit/test_mediawiki_extractor.py
+++ dags/hivemind_etl_helpers/tests/unit/test_mediawiki_extractor.py
@@ -31,7 +31,8 @@ class TestMediaWikiExtractor(unittest.TestCase):
         test_pages = ["Python_(programming_language)", "OpenAI"]
         documents = self.extractor.extract(page_ids=test_pages)
         self.assertEqual(len(documents), len(mock_response))
-        self.mock_reader.load_data.assert_called_once_with(pages=test_pages)
+        self.mock_reader.load_data.assert_called_once_with(pages=test_pages,
+                                                           auto_suggest=False)
 
     def test_extract_no_pages(self):
         """
@@ -52,4 +53,5 @@ class TestMediaWikiExtractor(unittest.TestCase):
 
         documents = self.extractor.extract(page_ids=invalid_pages)
         self.assertEqual(len(documents), 0)
-        self.mock_reader.load_data.assert_called_with(pages=invalid_pages)
+        self.mock_reader.load_data.assert_called_with(pages=invalid_pages,
+                                                      auto_suggest=False)
