37a9a8f31517e9a0c7c7ae1f028b93138fb9a202
Mohammad Amin <dadgaramin96@gmail.com>
2024-06-03 12:51:53 +0330

    feat: Added protocol support!

--- dags/github/credentials.py
+++ dags/github/credentials.py
@@ -14,13 +14,14 @@ def load_smart_proxy_url() -> str:
     """
     load_dotenv()
 
+    protocol = os.getenv("SMART_PROXY_PROTOCOL")
     user = os.getenv("SMART_PROXY_USER")
     password = os.getenv("SMART_PROXY_PASSWORD")
     host = os.getenv("SMART_PROXY_HOST")
 
-    if user is None or password is None or host is None:
+    if user is None or password is None or host is None or protocol is None:
         raise EnvironmentError("One of the smart proxy env variables was not set!")
 
-    url = f"http://{user}:{password}@{host}"
+    url = f"{protocol}://{user}:{password}@{host}"
 
     return url
