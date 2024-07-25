3247544a4d6797cfccd6e95764d5552312aff974
Mohammad Amin <dadgaramin96@gmail.com>
2024-07-25 17:25:54 +0330

    feat: Added violation detectino load test cases!

--- dags/violation_detection_helpers/load.py
+++ dags/violation_detection_helpers/load.py
@@ -11,7 +11,7 @@ class LoadPlatformLabeledData:
         self,
         platform_id: str,
         transformed_data: list[dict],
-    ) -> list[dict]:
+    ) -> None:
         """
         update `rawmemberactivities` by with their label
 
@@ -22,10 +22,5 @@ class LoadPlatformLabeledData:
         transformed_data : list[dict]
             the extracted data to be transformed
             the transformation here is to label the violation for texts
-
-        Returns
-        ----------
-        labeled_data : list[dict]
-            the same data but with a label for violation detection
         """
         raise NotImplementedError
