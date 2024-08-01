f87316ce6293e551c06aa5cdf841b5b0df2869e1
polux0 <afidelo@protonmail.com>
2024-08-01 11:41:25 +0200

    feat: raw data

--- dags/analyzer_helper/telegram/extract_raw_data.py
+++ dags/analyzer_helper/telegram/extract_raw_data.py
@@ -87,99 +87,22 @@ class ExtractRawInfo:
 
         return messages
 
-    def fetch_post_categories(self, post_ids):
-        """
-        Fetch categories associated with given post IDs.
-
-        :param post_ids: List of post IDs.
-        :return: List of dictionaries containing post categories.
-        """
-        query = """
-        MATCH (post:DiscoursePost)
-        WHERE post.id in $post_ids
-        MATCH (topic:DiscourseTopic)-[:HAS_POST]->(post)
-        OPTIONAL MATCH (category:DiscourseCategory)-[:HAS_TOPIC]->(topic)
-        RETURN
-        post.id AS post_id,
-        category.id AS category_id
-        """
-        with self.driver.session() as session:
-            result = session.run(query, post_ids=post_ids)
-            records = [record.data() for record in result]
-            self.driver.close()
-            return records
-
-    def get_latest_post_created_at(self, forum_endpoint: str) -> Optional[str]:
-        """
-        Fetches the created_at timestamp of the latest post from a specified forum.
-
-        Args:
-            uri (str): The URI to connect to the Neo4j database.
-            user (str): The username for the Neo4j database.
-            password (str): The password for the Neo4j database.
-            forum_endpoint (str): The endpoint of the Discourse forum.
-
-        Returns:
-            Optional[str]: The created_at timestamp of the latest post, or None if no posts are found.
-        """
-        query = """
-        MATCH (forum:DiscourseForum {endpoint: $forum_endpoint})
-        WITH forum
-        MATCH (topic:DiscourseTopic {forumUuid: forum.uuid})
-        MATCH (topic)-[:HAS_POST]->(post:DiscoursePost)
-        RETURN post.createdAt AS created_at
-        ORDER BY post.createdAt DESC
-        LIMIT 1
-        """
-        with self.driver.session() as session:
-            result = session.run(query, forum_endpoint=forum_endpoint)
-            latest_post = result.single()
-            self.driver.close()
-        if latest_post:
-            return latest_post["created_at"]
-        else:
-            return None
-
-    def combine_posts_with_categories(
-        self, post_details: List[Dict[str, any]], post_categories: List[Dict[str, any]]
-    ) -> List[Dict[str, any]]:
-        """
-        Combine post details with their respective categories.
-
-        :param post_details: List of dictionaries containing post details.
-        :param post_categories: List of dictionaries containing post categories.
-        :return: List of combined dictionaries.
-        """
-        category_dict = {category["post_id"]: category for category in post_categories}
-
-        combined_results = []
-        for post in post_details:
-            category = category_dict.get(
-                post["post_id"], {"category_id": None, "category_name": None}
-            )
-            post["category_id"] = category["category_id"]
-            combined_results.append(post)
-
-        return combined_results
-
     def fetch_raw_data(
         self, created_at: Optional[str] = None, comparison: Optional[str] = None
     ) -> list:
         """
-        Fetch and combine post details and categories.
+        Fetch telegram raw data.
 
         :param created_at: Optional datetime string to filter posts created after this date.
         :param comparison: Optional comparison operator, either 'gt' for greater than or 'gte' for greater than or equal to.
-        :return: List of combined dictionaries containing post details and categories.
+        :return: List of combined dictionaries containing telegram raw data.
         """
         if created_at and comparison:
-            post_details = self.fetch_post_details(created_at, comparison)
+            data = self.fetch_message_details(created_at, comparison)
         else:
-            post_details = self.fetch_post_details()
+            data = self.fetch_message_details()
 
-        post_ids = [post["post_id"] for post in post_details]
-        post_categories = self.fetch_post_categories(post_ids)
-        return self.combine_posts_with_categories(post_details, post_categories)
+        return data
 
     def extract(self, period: datetime, recompute: bool = False) -> list:
         """
@@ -187,7 +110,7 @@ class ExtractRawInfo:
 
         :param period: The datetime period to filter posts.
         :param recompute: Flag to indicate if recompute is needed.
-        :return: List of combined dictionaries containing post details and categories.
+        :return: List of combined dictionaries containing telegram raw data.
         """
         data = []
         if recompute:
@@ -204,17 +127,17 @@ class ExtractRawInfo:
                     if latest_activity_date
                     else None
                 )
-                # Convert latest_activity_date_datetime to ISO format with milliseconds
-                latest_activity_date_iso_format = (
+                # Convert latest_activity_date_datetime to unix timestmap format
+                latest_activity_date_unix_timestamp_format = (
                     self.converter.datetime_to_timestamp(latest_activity_date_datetime)
                     if latest_activity_date_datetime
                     else None
                 )
-                period_iso_format = self.converter.datetime_to_timestamp(period)
-                if latest_activity_date_iso_format >= period_iso_format:
-                    data = self.fetch_raw_data(latest_activity_date_iso_format, "gt")
+                period_unix_timestamp_format = self.converter.datetime_to_timestamp(period)
+                if latest_activity_date_unix_timestamp_format >= period_unix_timestamp_format:
+                    data = self.fetch_raw_data(latest_activity_date_unix_timestamp_format, "gt")
                 else:
-                    data = self.fetch_raw_data(period_iso_format, "gte")
+                    data = self.fetch_raw_data(period_unix_timestamp_format, "gte")
             else:
                 data = self.fetch_raw_data()
         return data
