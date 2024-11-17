87e6f95830446250c76debee81d6af19cc643c60
Mohammad Amin <dadgaramin96@gmail.com>
2024-11-17 14:45:20 +0330

    feat: Completed data ingestion! wip.

--- dags/hivemind_etl_helpers/website_etl.py
+++ dags/hivemind_etl_helpers/website_etl.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+import hashlib
 from hivemind_etl_helpers.ingestion_pipeline import CustomIngestionPipeline
 from llama_index.core import Document
 from hivemind_etl_helpers.src.db.website.crawlee_client import CrawleeClient
@@ -25,20 +28,82 @@ class WebsiteETL:
             self.community_id, collection_name=collection_name
         )
 
-    def extract(
+    async def extract(
           self, urls: list[str],
-    ) -> list:
-       """
-       extract data
-       """
-       extracted_data = self.crawlee_client.crawl(urls)
+    ) -> list[dict[str, Any]]:
+        """
+        Extract given urls
 
-       return extracted_data
+        Parameters
+        -----------
+        urls : list[str]
+            a list of urls
+
+        Returns
+        ---------
+        extracted_data : list[dict[str, Any]]
+            The crawled data from urls
+        """
+        extracted_data = await self.crawlee_client.crawl(urls)
+
+        return extracted_data
     
-    def transform(self, raw_data: list) -> list[Document]:
-       # transforming
-       pass
+    def transform(self, raw_data: list[dict[str, Any]]) -> list[Document]:
+        """
+        transform raw data to llama-index documents
+
+        Parameters
+        ------------
+        raw_data : list[dict[str, Any]]
+            crawled data
+
+        Returns
+        ---------
+        documents : list[llama_index.Document]
+            list of llama-index documents
+        """
+        documents: list[Document] = []
+
+        for data in raw_data:
+            doc_id = data["url"]
+            doc = Document(
+                doc_id=doc_id,
+                text=data["inner_text"],
+                metadata={
+                    "title": data["title"],
+                    "url": data["url"],
+                }
+            )
+            documents.append(doc)
+
+        return documents
 
     def load(self, documents: list[Document]) -> None:
-       # loading data into db
-       self.ingestion_pipeline.run_pipeline(docs=documents)
+        """
+        load the documents into the vector db
+
+        Parameters
+        -------------
+        documents: list[llama_index.Document]
+            the llama-index documents to be ingested
+        """
+        # loading data into db
+        self.ingestion_pipeline.run_pipeline(docs=documents)
+
+    
+    def _prepare_id(data: str) -> str:
+        """
+        hash the given data to prepare an id for the document
+
+        Parameters
+        -----------
+        data : str
+            the string data to be hashed
+
+        Returns
+        --------
+        hashed_data : str
+            data hashed using sha256 algorithm
+        """
+        hashed_data = hashlib.sha256(data.encode('utf-8'))
+        return hashed_data.hexdigest()
