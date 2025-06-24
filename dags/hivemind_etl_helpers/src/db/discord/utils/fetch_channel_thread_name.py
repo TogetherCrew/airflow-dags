253f9993ae1691bc110c1409131f213ba7f5c4cd
Mohammad Amin <dadgaramin96@gmail.com>
2025-06-24 10:54:33 +0330

    feat: update annotations!

--- dags/hivemind_etl_helpers/src/db/discord/utils/fetch_channel_thread_name.py
+++ dags/hivemind_etl_helpers/src/db/discord/utils/fetch_channel_thread_name.py
@@ -8,7 +8,20 @@ class FetchDiscordChannelThreadNames:
         self.channels_collection = self.db["channels"]
         self.threads_collection = self.db["threads"]
 
-    def fetch_discord_channel_name(self, channel_id: str) -> str:
+    def fetch_discord_channel_name(self, channel_id: str) -> str | None:
+        """
+        Fetch the name of a channel from the database.
+
+        Parameters:
+        --------------
+        channel_id : str
+            The ID of the channel to fetch the name of.
+
+        Returns:
+        ---------
+        str | None
+            The name of the channel, or None if the channel is not found.
+        """
         query = {"channelId": channel_id}
         channel = self.channels_collection.find_one(query)
         if channel is None:
@@ -16,7 +29,20 @@ class FetchDiscordChannelThreadNames:
         else:
             return channel["name"]
 
-    def fetch_discord_thread_name(self, thread_id: str) -> str:
+    def fetch_discord_thread_name(self, thread_id: str) -> str | None:
+        """
+        Fetch the name of a thread from the database.
+
+        Parameters:
+        --------------
+        thread_id : str
+            The ID of the thread to fetch the name of.
+
+        Returns:
+        ---------
+        str | None
+            The name of the thread, or None if the thread is not found.
+        """
         query = {"id": thread_id}
         thread = self.threads_collection.find_one(query)
         if thread is None:
